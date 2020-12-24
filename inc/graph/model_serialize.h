f0a37813aa8db596185a8f97702f7642361da951
zhangxiaokun <zhang.xiaokun@huawei.com>
2020-12-24 11:20:50 +0800

    Avoid copy of Model

--- inc/graph/model_serialize.h
+++ inc/graph/model_serialize.h
@@ -28,8 +28,8 @@ class ModelSerialize {
  public:
   Buffer SerializeModel(const Model &model, bool is_dump = false);
 
-  Model UnserializeModel(const uint8_t *data, size_t len);
-  Model UnserializeModel(ge::proto::ModelDef &model_def);
+  bool UnserializeModel(const uint8_t *data, size_t len, Model &model);
+  bool UnserializeModel(ge::proto::ModelDef &model_def, Model &model);
 
   Buffer SerializeGraph(const ComputeGraphPtr &graph);
 
