f2e172261489bdd94ee27772ec6531608c8bccad
wangwenhua1@huawei.com <wangwenhua1@huawei.com>
2020-12-30 19:38:43 +0800

    infershape cycle not update input desc

--- inc/external/register/register_fmk_types.h
+++ inc/external/register/register_fmk_types.h
@@ -38,11 +38,11 @@ enum FrameworkType {
 /// @brief  AI framework type to string
 ///
 const std::map<std::string, std::string> kFwkTypeToStr = {
-        {"0", "Caffe"},
-        {"1", "MindSpore"},
-        {"3", "TensorFlow"},
-        {"4", "Android_NN"},
-        {"5", "Onnx"}
+    {"0", "Caffe"},
+    {"1", "MindSpore"},
+    {"3", "TensorFlow"},
+    {"4", "Android_NN"},
+    {"5", "Onnx"}
 };
 }  // namespace domi
 
