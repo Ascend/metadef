d0264178a96fe4dc97f431e73d345fff88866546
zhaoxinxin <zhaoxinxin1@huawei.com>
2021-03-16 09:54:10 +0800

    modified:   graph/utils/transformer_utils.cc

--- graph/utils/transformer_utils.cc
+++ graph/utils/transformer_utils.cc
@@ -21,6 +21,7 @@
 #include "graph/utils/type_utils.h"
 #include "graph/utils/attr_utils.h"
 #include "inc/graph/debug/ge_attr_define.h"
+#include "expand_dimension.h"
 #include "transfer_shape_according_to_format.h"
 
 namespace ge {
@@ -111,6 +112,15 @@ bool NodeShapeTransUtils::UpdateFormatAndShape() {
     std::vector<int64_t> out_dims;
     ge::DataType dtype =  map_dtype_in_[i];
 
+    // FE set and Ge get for PadDimention
+    string infer_shape_type;
+    (void) AttrUtils::GetStr(*tensor_desc_input, ATTR_NAME_RESHAPE_INFER_TYPE, infer_shape_type);
+    bool is_success = transformer::ExpandDimension(op_desc_->GetType(), ori_format, curr_format, i,
+                                                   infer_shape_type, ori_shape_dims);
+    if (!is_success) {
+      GELOGE(GRAPH_FAILED, "Call expandDimension failed.");
+      return GRAPH_FAILED;
+    }
     transformer::ShapeAndFormat shape_and_format_info {ori_shape_dims, out_dims, ori_format, curr_format, dtype,
                                                        transformer::EN_IMPL_CUSTOM_TBE};
     shape_transfer.GetShapeAccordingToFormat(shape_and_format_info);
@@ -152,6 +162,15 @@ bool NodeShapeTransUtils::UpdateFormatAndShape() {
     std::vector<int64_t> out_dims;
     ge::DataType dtype =  tensor_desc_output->GetDataType();
 
+    // FE set and Ge get for PadDimention
+    string infer_shape_type;
+    (void) AttrUtils::GetStr(*tensor_desc_output, ATTR_NAME_RESHAPE_INFER_TYPE, infer_shape_type);
+    bool is_success = transformer::ExpandDimension(op_desc_->GetType(), curr_format, saved_format, i,
+                                                   infer_shape_type, ori_shape_dims);
+    if (!is_success) {
+      GELOGE(GRAPH_FAILED, "Call expandDimension failed.");
+      return GRAPH_FAILED;
+    }
     transformer::ShapeAndFormat shape_and_format_info {ori_shape_dims, out_dims, curr_format, saved_format, dtype,
                                                        transformer::EN_IMPL_CUSTOM_TBE};
     shape_transfer.GetShapeAccordingToFormat(shape_and_format_info);
