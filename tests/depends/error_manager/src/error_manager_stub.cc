3b1bdaef0f7ea4fc920c58e0ac73967e504d5820
koala_zhang <571700104@qq.com>
2021-03-16 19:31:18 +0800

    add ReportInterErrMessage stub

--- tests/depends/error_manager/src/error_manager_stub.cc
+++ tests/depends/error_manager/src/error_manager_stub.cc
@@ -37,6 +37,12 @@
   int ErrorManager::ReportErrMessage(std::string error_code, const std::map<std::string, std::string> &args_map) {
     return 0;
   }
+
+  int ErrorManager::ReportInterErrMessage(std::string error_code, const std::string &error_msg) {
+    return 0;
+  }
+
+
   const std::string kLogHeader("GeUtStub");
   const std::string &ErrorManager::GetLogHeader() {
   return kLogHeader;
