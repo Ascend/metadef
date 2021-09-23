e794d9e480669fb23bcc9379936e5cb738991be1
lipeijie <lipeijie@huawei.com>
2021-09-23 09:42:39 +0800

    optimize transform

--- third_party/transformer/src/transfer_range_according_to_format.cc
+++ third_party/transformer/src/transfer_range_according_to_format.cc
@@ -28,26 +28,26 @@ bool RangeTransferAccordingToFormat::GetRangeAccordingToFormat(RangeAndFormat &r
   /* The default new range is old range */
   std::vector<int64_t> range_upper_old;
   std::vector<int64_t> range_low_old;
-  std::vector<int64_t> range_upper_new;
-  std::vector<int64_t> range_low_new;
   for (auto &i : range_and_format_info.old_range) {
     range_low_old.emplace_back(i.first);
     range_upper_old.emplace_back(i.second);
   }
 
-  transformer::ShapeAndFormat shape_and_format_info_low {range_low_old, range_low_new, range_and_format_info.old_format,
-      range_and_format_info.new_format, range_and_format_info.current_data_type, range_and_format_info.op_impl_type};
-  transformer::ShapeAndFormat shape_and_format_info_upper {range_upper_old, range_upper_new, range_and_format_info.old_format,
-      range_and_format_info.new_format, range_and_format_info.current_data_type, range_and_format_info.op_impl_type};  
+  ge::GeShape shape_low(range_low_old);
+  ge::GeShape shape_upper(range_upper_old);
+  transformer::ShapeAndFormat shape_and_format_info_low {shape_low, range_and_format_info.old_format,
+      range_and_format_info.new_format, range_and_format_info.current_data_type};
+  transformer::ShapeAndFormat shape_and_format_info_upper {shape_upper, range_and_format_info.old_format,
+      range_and_format_info.new_format, range_and_format_info.current_data_type};
   ShapeTransferAccordingToFormat shape_transfer;
   bool res = (shape_transfer.GetShapeAccordingToFormat(shape_and_format_info_low) &&
       shape_transfer.GetShapeAccordingToFormat(shape_and_format_info_upper));
-  if (!res || (range_low_new.size() != range_upper_new.size())) {
+  if (!res || (shape_low.GetDimNum() != shape_upper.GetDimNum())) {
     return false;
   }
   range_and_format_info.new_range.clear();
   for (size_t i = 0; i < range_and_format_info.new_range.size(); ++i) {
-    range_and_format_info.new_range.emplace_back(range_low_new[i], range_upper_new[i]);
+    range_and_format_info.new_range.emplace_back(shape_low.GetDim(i), shape_upper.GetDim(i));
   }
   return res;
 }
