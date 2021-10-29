04f49f2b7d4404c0ecd53b740425e47117d83bb4
chentingting43 <chentingting43@hisilicon.com>
2021-10-29 18:09:10 +0800

    update log level

--- register/op_tiling/op_tiling.cc
+++ register/op_tiling/op_tiling.cc
@@ -363,7 +363,6 @@ ge::graphStatus TurnToOpParaCalculateV2(const ge::Node &node, OpRunInfoV2 &run_i
   ge::Operator op_param = ge::OpDescUtils::CreateOperatorFromNode(node.shared_from_this());
   bool ret = (tiling_func)(op_param, op_compile_info, run_info);
   if (ret) {
-    std::cout << "1111111111111111111" << std::endl;
     GELOGI("Op tiling v2 succeed. op_type:%s, op_name:%s", op_desc->GetType().c_str(), op_desc->GetName().c_str());
   } else {
     GELOGW("E19999", "Fail to call op tiling function v2 of op[%s, %s].",
