6802784c845e16f1387afc8e9c5136cb9b632c5d
chentingting43 <chentingting43@hisilicon.com>
2021-10-29 16:35:54 +0800

    update log level

--- register/op_tiling/op_tiling.cc
+++ register/op_tiling/op_tiling.cc
@@ -366,7 +366,7 @@ ge::graphStatus TurnToOpParaCalculateV2(const ge::Node &node, OpRunInfoV2 &run_i
     GELOGI("Op tiling v2 succeed. op_type:%s, op_name:%s", op_desc->GetType().c_str(), op_desc->GetName().c_str());
   } else {
     GELOGW("E19999", "Fail to call op tiling function v2 of op[%s, %s].",
-            op_desc->GetType().c_str(), op_desc->GetName().c_str());
+           op_desc->GetType().c_str(), op_desc->GetName().c_str());
     GELOGW("Fail to call op tiling function v2 of op[%s, %s].",
            op_desc->GetName().c_str(), op_desc->GetType().c_str());
   }
