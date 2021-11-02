0422a8117168c2657e27db8c9a4e65f9f87648dd
chentingting43 <chentingting43@hisilicon.com>
2021-11-02 09:14:36 +0800

    update misra

--- inc/register/graph_optimizer/buffer_fusion/buffer_fusion_pass_base.h
+++ inc/register/graph_optimizer/buffer_fusion/buffer_fusion_pass_base.h
@@ -41,9 +41,23 @@ class BufferFusionPassBase {
   virtual std::vector<BufferFusionPattern *> DefinePatterns() = 0;
   virtual Status GetFusionNodes(const BufferFusionMapping &mapping, std::vector<ge::NodePtr> &fusion_nodes);
   virtual Status CalcFusionOpSliceInfo(std::vector<ge::NodePtr> &fusion_nodes, OpCalcInfo &op_slice_info);
+#ifdef ONLY_COMPILE_OPEN_SRC
+  std::vector<ge::NodePtr> GetMatchedNodes(const BufferFusionMapping &mapping);
+#else
   static std::vector<ge::NodePtr> GetMatchedNodes(const BufferFusionMapping &mapping);
-  static std::vector<ge::NodePtr> GetMatchedNodesByDescName(const std::string &desc_name, const BufferFusionMapping &mapping);
+#endif
+#ifdef ONLY_COMPILE_OPEN_SRC
+  std::vector<ge::NodePtr> GetMatchedNodesByDescName(const std::string &desc_name,
+                                                     const BufferFusionMapping &mapping);
+#else
+  static std::vector<ge::NodePtr> GetMatchedNodesByDescName(const std::string &desc_name,
+                                                            const BufferFusionMapping &mapping);
+#endif
+#ifdef ONLY_COMPILE_OPEN_SRC
+  ge::NodePtr GetMatchedHeadNode(const std::vector<ge::NodePtr> &matched_nodes);
+#else
   static ge::NodePtr GetMatchedHeadNode(const std::vector<ge::NodePtr> &matched_nodes);
+#endif
 
   void SetName(const std::string &name) { name_ = name; }
 
