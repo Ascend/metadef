29a43e840ba8f6c0e74837108abcbfe35ecd2ef3
d00502325 <duhongxiang@huawei.com>
2021-11-23 20:19:07 +0800

    dynamic shape

--- tests/ut/register/testcase/register_optiling_unittest.cc
+++ tests/ut/register/testcase/register_optiling_unittest.cc
@@ -18,10 +18,11 @@
 #include <iostream>
 #include "register/op_tiling_registry.h"
 #include "op_tiling/op_tiling.cc"
-
+#include "common/sgt_type.h"
+#include "graph_builder_utils.h"
 using namespace std;
 using namespace ge;
-
+using namespace fe;
 namespace optiling {
 using ByteBuffer = std::stringstream;
 class RegisterOpTilingUT : public testing::Test {
@@ -126,4 +127,175 @@ TEST_F(RegisterOpTilingUT, te_op_paras_test) {
   op_param.var_attrs.GetData("some_int_attr", "Int32", size);
   op_param.var_attrs.GetData("some_int_vec", "ListInt32", size);
 }
+
+bool op_tiling_stub(const Operator &op, const utils::OpCompileInfo &compile_info, utils::OpRunInfo &run_info) {
+  return true;
+}
+
+REGISTER_OP_TILING_V2(ReluV2, op_tiling_stub);
+
+TEST_F(RegisterOpTilingUT, OpFftsCalculateV2_1) {
+  auto root_builder = ut::GraphBuilder("root");
+  const auto &node = root_builder.AddNode("relu", "ReluV2", 1, 1);
+  OpDescPtr op_desc = node->GetOpDesc();
+  ThreadSliceMapPtr slice_info_ptr;
+  slice_info_ptr = std::make_shared<ThreadSliceMap>();
+  DimRange dim;
+  dim.lower = 0;
+  dim.higher = 1;
+  vector<DimRange> vec_1;
+  vec_1.push_back(dim);
+  vector<vector<DimRange>> vec_2;
+  vec_2.push_back(vec_1);
+  vec_2.push_back(vec_1);
+  slice_info_ptr->thread_scope_id = 1;
+  slice_info_ptr->thread_id = 2222;
+  slice_info_ptr->thread_mode = true;
+  slice_info_ptr->parallel_window_size = 2;
+  slice_info_ptr->slice_instance_num = 2;
+  slice_info_ptr->input_tensor_slice.push_back(vec_2);
+  slice_info_ptr->input_tensor_slice.push_back(vec_2);
+  slice_info_ptr->output_tensor_slice.push_back(vec_2);
+  slice_info_ptr->output_tensor_slice.push_back(vec_2);
+  (void)node->GetOpDesc()->SetExtAttr(fe::SGT_STRUCT_INFO, slice_info_ptr);
+  GeShape shape({4,1,3,4,16});
+  GeTensorDesc tensor_desc(shape, ge::FORMAT_NCHW, ge::DT_FLOAT);
+  op_desc->AddInputDesc("x", tensor_desc);
+  op_desc->AddOutputDesc("y", tensor_desc);
+  std::vector<OpRunInfoV2> op_run_info;
+  ge::graphStatus ret = OpFftsCalculateV2(*node, op_run_info);
+  EXPECT_EQ(ret, ge::GRAPH_FAILED);
+  string compile_info_key = "compile_info_key";
+  string compile_info_json = "compile_info_json";
+  (void)ge::AttrUtils::SetStr(op_desc, COMPILE_INFO_KEY, compile_info_key);
+  (void)ge::AttrUtils::SetStr(op_desc, COMPILE_INFO_JSON, compile_info_json);
+  auto dstAnchor = node->GetInDataAnchor(0);
+  ge::AnchorUtils::SetStatus(dstAnchor, ge::ANCHOR_DATA);
+  ret = OpFftsCalculateV2(*node, op_run_info);
+  EXPECT_EQ(ret, ge::GRAPH_SUCCESS);
+}
+
+// slice instance over
+TEST_F(RegisterOpTilingUT, OpFftsCalculateV2_2) {
+  auto root_builder = ut::GraphBuilder("root");
+  const auto &node = root_builder.AddNode("relu", "ReluV2", 1, 1);
+  OpDescPtr op_desc = node->GetOpDesc();
+  ThreadSliceMapPtr slice_info_ptr;
+  slice_info_ptr = std::make_shared<ThreadSliceMap>();
+  DimRange dim;
+  dim.lower = 0;
+  dim.higher = 1;
+  vector<DimRange> vec_1;
+  vec_1.push_back(dim);
+  vector<vector<DimRange>> vec_2;
+  vec_2.push_back(vec_1);
+  vec_2.push_back(vec_1);
+  slice_info_ptr->thread_scope_id = 1;
+  slice_info_ptr->thread_id = 2222;
+  slice_info_ptr->thread_mode = true;
+  slice_info_ptr->parallel_window_size = 2;
+  slice_info_ptr->slice_instance_num = 4;
+  slice_info_ptr->input_tensor_slice.push_back(vec_2);
+  slice_info_ptr->input_tensor_slice.push_back(vec_2);
+  slice_info_ptr->output_tensor_slice.push_back(vec_2);
+  slice_info_ptr->output_tensor_slice.push_back(vec_2);
+  (void)node->GetOpDesc()->SetExtAttr(fe::SGT_STRUCT_INFO, slice_info_ptr);
+  GeShape shape({4,1,3,4,16});
+  GeTensorDesc tensor_desc(shape);
+  op_desc->AddInputDesc("x", tensor_desc);
+  op_desc->AddOutputDesc("y", tensor_desc);
+  string compile_info_key = "compile_info_key";
+  string compile_info_json = "compile_info_json";
+  (void)ge::AttrUtils::SetStr(op_desc, COMPILE_INFO_KEY, compile_info_key);
+  (void)ge::AttrUtils::SetStr(op_desc, COMPILE_INFO_JSON, compile_info_json);
+  std::vector<OpRunInfoV2> op_run_info;
+  ge::graphStatus ret = OpFftsCalculateV2(*node, op_run_info);
+  EXPECT_EQ(ret, ge::GRAPH_FAILED);
+}
+
+TEST_F(RegisterOpTilingUT, UpDateNodeShapeBySliceInfo1) {
+  auto root_builder = ut::GraphBuilder("root");
+  const auto &node = root_builder.AddNode("relu", "ReluV2", 1, 1);
+  OpDescPtr op_desc = node->GetOpDesc();
+  ThreadSliceMapPtr slice_info_ptr;
+  slice_info_ptr = std::make_shared<ThreadSliceMap>();
+  DimRange dim;
+  dim.lower = 0;
+  dim.higher = 1;
+  vector<DimRange> vec_1;
+  vec_1.push_back(dim);
+  vector<vector<DimRange>> vec_2;
+  vector<vector<DimRange>> vec_3;
+  vec_2.push_back(vec_1);
+  vec_2.push_back(vec_1);
+  vec_3.push_back(vec_1);
+  slice_info_ptr->thread_scope_id = 1;
+  slice_info_ptr->thread_id = 2222;
+  slice_info_ptr->thread_mode = true;
+  slice_info_ptr->parallel_window_size = 2;
+  slice_info_ptr->slice_instance_num = 2;
+  slice_info_ptr->input_tensor_slice.push_back(vec_2);
+  slice_info_ptr->input_tensor_slice.push_back(vec_2);
+  slice_info_ptr->output_tensor_slice.push_back(vec_3);
+  (void)node->GetOpDesc()->SetExtAttr(fe::SGT_STRUCT_INFO, slice_info_ptr);
+  GeShape shape({4,1,3,4,16});
+  GeTensorDesc tensor_desc(shape);
+  op_desc->AddInputDesc("x", tensor_desc);
+  vector<vector<int64_t>> ori_shape;
+  vector<uint32_t> tmp = {};
+  vector<uint32_t> tmp1 = { 0, 2};
+  vector<vector<uint32_t>> in_out_idx;
+  in_out_idx.push_back(tmp);
+  in_out_idx.push_back(tmp1);
+  auto ret = UpDateNodeShapeBySliceInfo(slice_info_ptr, op_desc, 2, ori_shape, in_out_idx);
+  EXPECT_EQ(ret, ge::GRAPH_FAILED);
+  op_desc->AddOutputDesc("y", tensor_desc);
+  ret = UpDateNodeShapeBySliceInfo(slice_info_ptr, op_desc, 0, ori_shape, in_out_idx);
+  EXPECT_EQ(ret, ge::GRAPH_FAILED);
+}
+
+TEST_F(RegisterOpTilingUT, UpDateNodeShapeBySliceInfo2) {
+  auto root_builder = ut::GraphBuilder("root");
+  const auto &node = root_builder.AddNode("relu", "ReluV2", 1, 1);
+  OpDescPtr op_desc = node->GetOpDesc();
+  ThreadSliceMapPtr slice_info_ptr;
+  slice_info_ptr = std::make_shared<ThreadSliceMap>();
+  DimRange dim;
+  dim.lower = 0;
+  dim.higher = 1;
+  vector<DimRange> vec_1;
+  vec_1.push_back(dim);
+  vector<vector<DimRange>> vec_2;
+  vec_2.push_back(vec_1);
+  vec_2.push_back(vec_1);
+  slice_info_ptr->thread_scope_id = 1;
+  slice_info_ptr->thread_id = 2222;
+  slice_info_ptr->thread_mode = true;
+  slice_info_ptr->parallel_window_size = 2;
+  slice_info_ptr->slice_instance_num = 2;
+  slice_info_ptr->input_tensor_slice.push_back(vec_2);
+  slice_info_ptr->input_tensor_slice.push_back(vec_2);
+  slice_info_ptr->output_tensor_slice.push_back(vec_2);
+  slice_info_ptr->output_tensor_slice.push_back(vec_2);
+  GeShape shape({4,1,3,4,16});
+  GeTensorDesc tensor_desc(shape);
+  op_desc->AddInputDesc("x", tensor_desc);
+  op_desc->AddOutputDesc("y", tensor_desc);
+  vector<vector<int64_t>> ori_shape;
+  vector<uint32_t> tmp = {0, 1, 2};
+  vector<uint32_t> tmp1 = { 0, 2};
+  vector<vector<uint32_t>> in_out_idx;
+  in_out_idx.push_back(tmp);
+  in_out_idx.push_back(tmp1);
+  auto ret = UpDateNodeShapeBySliceInfo(slice_info_ptr, op_desc, 0, ori_shape, in_out_idx);
+  EXPECT_EQ(ret, ge::GRAPH_FAILED);
+  vector<vector<uint32_t>> in_out_idx1;
+  in_out_idx1.push_back(tmp1);
+  in_out_idx1.push_back(tmp);
+  ret = UpDateNodeShapeBySliceInfo(slice_info_ptr, op_desc, 1, ori_shape, in_out_idx);
+  EXPECT_EQ(ret, ge::GRAPH_FAILED);
+  ret = UpDateNodeShapeBack(op_desc, ori_shape, in_out_idx);
+  EXPECT_EQ(ret, ge::GRAPH_FAILED);
+}
+
 }  // namespace ge
