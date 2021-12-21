97cdd5b11c4432b2e6611f7bb1af5751c248cb72
wx1004905 <xuke77@hisilicon.com>
2021-12-21 09:53:15 +0800

    Add UT for graph/compile_cache_policy at 20211220.

--- tests/ut/graph/testcase/compile_cache_policy_hasher_unittest.cc
+++ tests/ut/graph/testcase/compile_cache_policy_hasher_unittest.cc
@@ -22,6 +22,41 @@ class UtestCompileCachePolicyHasher : public testing::Test {
   void TearDown() {}
 };
 
+TEST_F(UtestCompileCachePolicyHasher, TestBinaryHolderOnly) {
+  uint8_t data1[9] = {0U,1U,2U,3U,4U,5U,6U,7U,8U};
+  uint8_t data2[9] = {0U,1U,2U,3U,4U,5U,7U,9U,11U};
+  uint8_t data3[9] = {0U,1U,2U,3U,4U,5U,7U,9U,11U};
+
+  BinaryHolder holder1 = BinaryHolder();
+  holder1.SharedFrom(data1, 1);
+
+  const void *dataPtr = holder1.GetDataPtr();
+  ASSERT_NE(dataPtr, nullptr);
+  size_t size1 = holder1.GetDataLen();
+  ASSERT_EQ(size1, 1UL);
+
+  BinaryHolder holder2(holder1);
+  BinaryHolder holder3 = BinaryHolder();
+  holder3.SharedFrom(data2, 9);
+  BinaryHolder holder4 = BinaryHolder();
+  holder4.SharedFrom(data3, 9);
+  BinaryHolder holder5 = BinaryHolder();
+  holder5.SharedFrom(nullptr, 0);
+  BinaryHolder holder6 = BinaryHolder();
+  holder6.SharedFrom(data1, 9);
+
+  bool b1 = (holder1 != holder5);
+  ASSERT_EQ(b1, false);
+  bool b2 = (holder1 != holder2);
+  ASSERT_EQ(b2, false);
+  bool b3 = (holder3 != holder4);
+  ASSERT_EQ(b3, false);
+  bool b4 = (holder3 != holder6);
+  ASSERT_EQ(b4, true);
+  bool b5 = (holder4 != holder5);
+  ASSERT_EQ(b5, false);
+}
+
 TEST_F(UtestCompileCachePolicyHasher, GetCacheDescHashWithoutShape) {
   int64_t uid = 100UL;
   SmallVector<ShapeType, kDefaultMaxInputNum> shapes = {{2,3,4}};
