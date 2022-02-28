8b358f7c702a297a9d55ff906d6db2558b5f2e1c
缪驰 <miaochi1@hisilicon.com>
2022-02-28 16:47:23 +0800

    review modify

--- graph/utils/graph_utils.cc
+++ graph/utils/graph_utils.cc
@@ -3149,17 +3149,17 @@ void GraphUtils::InheritOriginalAttr(const ComputeGraphPtr &src_compute_graph,
 }
 
 CycleDetectorPtr GraphUtils::CreateCycleDetector(const ComputeGraphPtr &graph) {
-  CycleDetectorPtr detector_inner = std::unique_ptr<CycleDetector>(new (std::nothrow) CycleDetector());
-  if (detector_inner == nullptr) {
+  CycleDetectorPtr detector = std::unique_ptr<CycleDetector>(new (std::nothrow) CycleDetector());
+  if (detector == nullptr) {
     GELOGW("Fail to create cycle detector. Return null.");
     return nullptr;
   }
-  const auto ret = detector_inner->Init(graph);
+  const auto ret = detector->Init(graph);
   if (ret != SUCCESS) {
     GELOGW("Fail to init cycle detector. Return null.");
     return nullptr;
   }
-  return detector_inner;
+  return detector;
 }
 
 ///
