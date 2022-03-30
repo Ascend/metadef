54ef9a82d385e4be2d53b9a532079aef74741d10
CLAY-panjw <1330286576@qq.com>
2022-03-30 11:42:15 +0800

    for overflow detection

--- graph/ge_attr_define.cc
+++ graph/ge_attr_define.cc
@@ -1310,8 +1310,8 @@ const std::string ATTR_NAME_OP_READ_WRITE_INDEX = "_op_read_write_index";
 const std::string ATTR_NAME_MEM_RELEASE_FIRST_REUSE_FIRST = "_mem_release_first_reuse_first";
 
 // for support overflow detection
-const std::string GLOBLEWORKSPACE_STATUS = "_globleworkspace_status";
-const std::string GLOBLEWORKSPACE_STATUS_BYTES = "_globleworkspace_status_bytes";
+const std::string GLOBALWORKSPACE_SPEC_WORKSPACE = "globalworkspace_spec_workspace";
+const std::string GLOBALWORKSPACE_SPEC_WORKSPACE_BYTES = "globalworkspace_spec_workspace_bytes";
 
 // for value depend
 const std::string ATTR_NAME_VALUE_DEPEND = "_is_value_depend";
