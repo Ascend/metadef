3ea4ba8a4040b30610cfa8d3c4385460099c001f
韩健 <hanjian27@h-partners.com>
2022-06-10 12:02:37 +0000

    !2080 add string for fuzz compile Merge pull request !2080 from 韩健/mybranch

--- graph/ge_attr_define.cc
+++ graph/ge_attr_define.cc
@@ -1229,6 +1229,7 @@ const std::string ATTR_NAME_FUZZ_OUTPUTS_SUPPORTED_ATTRS = "_outputs_support_inf
 const std::string ATTR_NAME_FUZZ_IS_HIGH_PERFORMANCE_ATTRS = "_is_high_performance";
 const std::string ATTR_NAME_IS_ORIGINAL_INPUT = "_is_original_input";
 const std::string ATTR_NAME_IS_OP_GENERALIZED = "_is_op_generalized";
+const std::string ATTR_NAME_IS_DYNAMIC_MODEL = "_is_dynamic_model";
 
 // buffer pool allocator
 const std::string ATTR_NAME_BUFFER_POOL_ID = "_buffer_pool_id";
