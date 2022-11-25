b31be7aad5e6217eb70583dbc9f35a3a2d9c320a
LiHengjia <lihengjia@huawei.com>
2022-11-25 08:49:57 +0000

    !2781 AR00022DQ9 avgpool_update单算子 Merge pull request !2781 from LiHengjia/master

--- inc/register/graph_optimizer/buffer_fusion/buffer_fusion_constant.h
+++ inc/register/graph_optimizer/buffer_fusion/buffer_fusion_constant.h
@@ -58,6 +58,7 @@ const std::string OP_PATTERN_CONV3D_BACKPROP_INPUT = "Conv3d_backprop_input";
 const std::string OP_PATTERN_CONV_BACKPROP_FILTER = "Conv2d_backprop_filter";
 const std::string OP_PATTERN_GEMM = "GEMM";
 const std::string OP_PATTERN_FIXPIPE = "fixpipe";
+const std::string OP_PATTERN_AVGPOOLUPDATE = "AvgPoolUpdate";
 const std::vector<std::string> OP_PATTERN_VEC{OP_PATTERN_ELEMWISE,
                                               OP_PATTERN_COMMONREDUCE,
                                               OP_PATTERN_BROAD_CAST,
@@ -89,7 +90,8 @@ const std::vector<std::string> OP_PATTERN_VEC{OP_PATTERN_ELEMWISE,
                                               OP_PATTERN_CONV3D_BACKPROP_INPUT,
                                               OP_PATTERN_CONV_BACKPROP_FILTER,
                                               OP_PATTERN_GEMM,
-                                              OP_PATTERN_FIXPIPE
+                                              OP_PATTERN_FIXPIPE,
+                                              OP_PATTERN_AVGPOOLUPDATE
 };
 }  // namespace fe
 
