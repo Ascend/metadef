c933b1950f71968197d688e6705cc21be4455308
朱晶晶 <zhujingjing6@huawei.com>
2023-03-23 11:06:51 +0000

    !3329 change auto_batch to count_batch Merge pull request !3329 from 朱晶晶/ge_dev

--- graph/flow_graph/data_flow_attr_define.cc
+++ graph/flow_graph/data_flow_attr_define.cc
@@ -25,13 +25,13 @@ const char *const ATTR_NAME_DATA_FLOW_INPUT = "input";
 const char *const ATTR_NAME_DATA_FLOW_OUTPUT = "output";
 
 // For count batch
-const char *const ATTR_NAME_COUNT_BATCH_BATCH_SIZE = "_auto_batch_batch_size";
-const char *const ATTR_NAME_COUNT_BATCH_SLIDE_STRIDE = "_auto_batch_slide_stride";
-const char *const ATTR_NAME_COUNT_BATCH_TIMEOUT = "_auto_batch_timeout";
-const char *const ATTR_NAME_COUNT_BATCH_BATCH_DIM = "_auto_batch_dim";
-const char *const ATTR_NAME_COUNT_BATCH_FLAG = "_auto_batch_flag";
-const char *const ATTR_NAME_COUNT_BATCH_PADDING = "_auto_batch_padding";
-const char *const ATTR_NAME_COUNT_BATCH_DROP_REMAINDER = "_auto_batch_drop_remainder";
+const char *const ATTR_NAME_COUNT_BATCH_BATCH_SIZE = "_count_batch_batch_size";
+const char *const ATTR_NAME_COUNT_BATCH_SLIDE_STRIDE = "_count_batch_slide_stride";
+const char *const ATTR_NAME_COUNT_BATCH_TIMEOUT = "_count_batch_timeout";
+const char *const ATTR_NAME_COUNT_BATCH_BATCH_DIM = "_count_batch_dim";
+const char *const ATTR_NAME_COUNT_BATCH_FLAG = "_count_batch_flag";
+const char *const ATTR_NAME_COUNT_BATCH_PADDING = "_count_batch_padding";
+const char *const ATTR_NAME_COUNT_BATCH_DROP_REMAINDER = "_count_batch_drop_remainder";
 
 // For time batch
 const char *const ATTR_NAME_TIME_BATCH_TIME_WINDOW = "_time_batch_window";
