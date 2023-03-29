eace3938c4e8e7eb889668db6926107f764df0df
huanruizhi <huanruizhi@hisilicon.com>
2023-03-29 12:37:45 +0000

    !3319 topo排序在内存优先模式下优化数据结构 Merge pull request !3319 from huanruizhi/dev

--- tests/ut/graph/testcase/compute_graph_unittest.cc
+++ tests/ut/graph/testcase/compute_graph_unittest.cc
@@ -96,6 +96,43 @@ ge::ComputeGraphPtr BuildMainGraphWithIf(const std::string &name) {
 
   return main_graph;
 }
+/*
+ *          netoutput
+ *         |    \    \
+ *       node4 node5 node6
+ *       |      \
+ *     node2  node3
+ *      \    /
+ *      node1
+ */
+ge::ComputeGraphPtr BuildNormalGraph(const std::string &name) {
+  auto builder = ge::ut::GraphBuilder(name);
+  const auto &node1 = builder.AddNode("node1", "node1", 0, 2);
+  const auto &node2 = builder.AddNode("node2", "node2", 1, 1);
+  const auto &node3 = builder.AddNode("node3", "node3", 1, 1);
+  const auto &node4 = builder.AddNode("node4", "node4", 1, 1);
+  const auto &node5 = builder.AddNode("node5", "node5", 1, 1);
+  const auto &node6 = builder.AddNode("node6", "node6", 0, 1);
+  const auto &netoutput = builder.AddNode("netoutput", "netoutput", 3, 1);
+
+  builder.AddDataEdge(node1, 0, node2, 0);
+  builder.AddDataEdge(node1, 1, node3, 0);
+  builder.AddDataEdge(node2, 0, node4, 0);
+  builder.AddDataEdge(node3, 0, node5, 1);
+  builder.AddDataEdge(node4, 0, netoutput, 0);
+  builder.AddDataEdge(node5, 0, netoutput, 1);
+  builder.AddDataEdge(node6, 0, netoutput, 2);
+
+  builder.AddControlEdge(node1, node2);
+  builder.AddControlEdge(node1, node3);
+  builder.AddControlEdge(node2, node4);
+  builder.AddControlEdge(node3, node5);
+  builder.AddControlEdge(node4, netoutput);
+  builder.AddControlEdge(node5, netoutput);
+  builder.AddControlEdge(node6, netoutput);
+
+  return builder.GetGraph();
+}
 }
 
 namespace ge
@@ -408,32 +445,8 @@ TEST_F(UtestComputeGraph, TopologicalSorting_success) {
  */
 TEST_F(UtestComputeGraph, TopologicalSortingMode_success) {
   std::map<std::string, std::string> options_map;
-  auto builder = ut::GraphBuilder("graph");
-  const auto &node1 = builder.AddNode("node1", "node1", 0, 2);
-  const auto &node2 = builder.AddNode("node2", "node2", 1, 1);
-  const auto &node3 = builder.AddNode("node3", "node3", 1, 1);
-  const auto &node4 = builder.AddNode("node4", "node4", 1, 1);
-  const auto &node5 = builder.AddNode("node5", "node5", 1, 1);
-  const auto &node6 = builder.AddNode("node6", "node6", 0, 1);
-  const auto &netoutput = builder.AddNode("netoutput", "netoutput", 3, 1);
 
-  builder.AddDataEdge(node1, 0, node2, 0);
-  builder.AddDataEdge(node1, 1, node3, 0);
-  builder.AddDataEdge(node2, 0, node4, 0);
-  builder.AddDataEdge(node3, 0, node5, 1);
-  builder.AddDataEdge(node4, 0, netoutput, 0);
-  builder.AddDataEdge(node5, 0, netoutput, 1);
-  builder.AddDataEdge(node6, 0, netoutput, 2);
-
-  builder.AddControlEdge(node1, node2);
-  builder.AddControlEdge(node1, node3);
-  builder.AddControlEdge(node2, node4);
-  builder.AddControlEdge(node3, node5);
-  builder.AddControlEdge(node4, netoutput);
-  builder.AddControlEdge(node5, netoutput);
-  builder.AddControlEdge(node6, netoutput);
-
-  auto graph = builder.GetGraph();
+  auto graph = BuildNormalGraph("test_topo_graph");
   EXPECT_EQ(graph->TopologicalSorting(), GRAPH_SUCCESS);
   std::vector<std::string> expected_bfs_names = {"node1", "node2", "node3", "node4", "node5", "node6", "netoutput"};
   std::vector<std::string> expected_dfs_names = {"node1", "node3", "node5", "node2", "node4", "node6", "netoutput"};
@@ -460,6 +473,63 @@ TEST_F(UtestComputeGraph, TopologicalSortingMode_success) {
   EXPECT_EQ(graph->TopologicalSorting(), GRAPH_SUCCESS);
 
   EXPECT_EQ(bfs_names, expected_bfs_names);
+  EXPECT_EQ(dfs_names, expected_dfs_names);
+}
+
+TEST_F(UtestComputeGraph, BFSTopologicalSortingInPriorityMode_success) {
+  std::map<std::string, std::string> options_map;
+
+  auto graph = BuildNormalGraph("test_bfs_topo_graph");
+  EXPECT_EQ(graph->TopologicalSorting(), GRAPH_SUCCESS);
+  std::vector<std::string> expected_bfs_names = {"node1", "node3", "node2", "node4", "node5", "node6", "netoutput"};
+  std::vector<std::string> bfs_names;
+  options_map.emplace("ge.topoSortingMode", "0");
+  options_map.emplace("ge.exec.memoryOptimizationPolicy", "MemoryPriority");
+  GetThreadLocalContext().SetGraphOption(options_map);
+  EXPECT_EQ(graph->TopologicalSorting(), GRAPH_SUCCESS);
+  const auto &graph_bfs_topo = graph->GetAllNodes();
+  for (auto &node : graph_bfs_topo) {
+    bfs_names.push_back(node->GetName());
+  }
+
+  EXPECT_EQ(bfs_names, expected_bfs_names);
+}
+
+TEST_F(UtestComputeGraph, DFSTopologicalSortingInPriorityMode_success) {
+  std::map<std::string, std::string> options_map;
+
+  auto graph = BuildNormalGraph("test_dfs_topo_graph");
+  EXPECT_EQ(graph->TopologicalSorting(), GRAPH_SUCCESS);
+  std::vector<std::string> expected_dfs_names = {"node6", "node1", "node3", "node2", "node4", "node5", "netoutput"};
+  std::vector<std::string> dfs_names;
+  options_map.emplace("ge.topoSortingMode", "1");
+  options_map.emplace("ge.exec.memoryOptimizationPolicy", "MemoryPriority");
+  GetThreadLocalContext().SetGraphOption(options_map);
+  EXPECT_EQ(graph->TopologicalSorting(), GRAPH_SUCCESS);
+  const auto &graph_dfs_topo = graph->GetAllNodes();
+  for (auto &node : graph_dfs_topo) {
+    dfs_names.push_back(node->GetName());
+  }
+
+  EXPECT_EQ(dfs_names, expected_dfs_names);
+}
+
+TEST_F(UtestComputeGraph, ReverseDfsTopologicalSortingInPriorityMode_success) {
+  std::map<std::string, std::string> options_map;
+
+  auto graph = BuildNormalGraph("test_reverse_dfs_topo_graph");
+  EXPECT_EQ(graph->TopologicalSorting(), GRAPH_SUCCESS);
+  std::vector<std::string> expected_dfs_names = {"node6", "node1", "node2", "node4", "node3", "node5", "netoutput"};
+  std::vector<std::string> dfs_names;
+  options_map.emplace("ge.topoSortingMode", "1");
+  options_map.emplace("ge.exec.memoryOptimizationPolicy", "MemoryPriority");
+  GetThreadLocalContext().SetGraphOption(options_map);
+  EXPECT_EQ(graph->TopologicalSortingGraph(true), GRAPH_SUCCESS);
+  const auto &graph_dfs_topo = graph->GetAllNodes();
+  for (auto &node : graph_dfs_topo) {
+    dfs_names.push_back(node->GetName());
+  }
+
   EXPECT_EQ(dfs_names, expected_dfs_names);
 }
 
