2c4b69c525f49fc726cd82f00a666c80280501c1
yanqingshang <yanqingshang@huawei.com>
2023-05-09 14:59:59 +0000

    !3561 告警清理 Merge pull request !3561 from yanqingshang/ge_dev

--- inc/register/tensor_assign.h
+++ inc/register/tensor_assign.h
@@ -109,7 +109,7 @@ class TensorAssign {
         // real value and complex value are stored spaced apart, so use 2 and 1 to store in the correct addr.
         const int64_t value_l = val_size - kComplexWidth;
         GE_ASSERT_EQ(ge::IntegerChecker<int32_t>::Compat(value_l), true);
-        for (int64_t i = val_size; i < count; i = i + kComplexWidth) {
+        for (int64_t i = val_size; i < count; i += kComplexWidth) {
           addr[static_cast<size_t>(i)] = val_vector.Get(static_cast<int32_t>(value_l));
           addr[static_cast<size_t>(i) + 1UL] = val_vector.Get(static_cast<int32_t>(value_r));
         }
@@ -120,7 +120,7 @@ class TensorAssign {
       }
     } else {
       if (is_complex) {
-        for (int64_t i = 0; i < count; i = i + kComplexWidth) {
+        for (int64_t i = 0; i < count; i += kComplexWidth) {
           addr[static_cast<size_t>(i)] = val_vector.Get(0);
           addr[static_cast<size_t>(i) + 1UL] = val_vector.Get(1);
         }
