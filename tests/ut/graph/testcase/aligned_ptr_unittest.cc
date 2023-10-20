a112f272c3bd6591504b9c0fc48b080259c464fc
詹隽 <zhanjun6@hisilicon.com>
2023-10-20 09:39:25 +0000

    !4226 support reset data in aliginptr Merge pull request !4226 from 詹隽/reset_data

--- tests/ut/graph/testcase/aligned_ptr_unittest.cc
+++ tests/ut/graph/testcase/aligned_ptr_unittest.cc
@@ -14,11 +14,11 @@
  * limitations under the License.
  */
 
-#include <gtest/gtest.h>
 #include "common/util/mem_utils.h"
+#include <gtest/gtest.h>
+#include <malloc.h>
 #include <memory>
 #include <stdio.h>
-#include <malloc.h>
 
 #define private public
 #define protected public
@@ -27,33 +27,44 @@
 #undef protected
 #include "graph/aligned_ptr.h"
 
-namespace ge
-{
-  class UtestAlignedPtr : public testing::Test {
-   protected:
-   void SetUp() {}
-   void TearDown() {}
+namespace ge {
+class UtestAlignedPtr : public testing::Test {
+ protected:
+  void SetUp() {}
+  void TearDown() {}
+};
+
+TEST_F(UtestAlignedPtr, Reset_success) {
+  auto aligned_ptr = MakeShared<AlignedPtr>(6);
+  uint64_t aligned_data = reinterpret_cast<uintptr_t>(aligned_ptr->Get());
+  uint64_t base_data = reinterpret_cast<uintptr_t>(aligned_ptr->base_.get());
+  auto output = aligned_ptr->Reset();
+  uint64_t output_data = reinterpret_cast<uintptr_t>(output.get());
+  uint64_t output_base = reinterpret_cast<uintptr_t>(aligned_ptr->base_.get());
+  ASSERT_EQ(aligned_data, output_data);
+  ASSERT_EQ(output_base, 0);
+
+  auto reset_null = aligned_ptr->Reset(nullptr, nullptr);
+  ASSERT_EQ(reset_null, nullptr);
+  ASSERT_EQ(aligned_data, output_data);
+
+  uint8_t *data_ptr = new uint8_t(10);
+  auto deleter = [](uint8_t *ptr) {
+    delete ptr;
+    ptr = nullptr;
   };
-
-  TEST_F(UtestAlignedPtr, Reset_success) {
-   auto aligned_ptr = MakeShared<AlignedPtr>(6);
-   uint64_t aligned_data = reinterpret_cast<uintptr_t>(aligned_ptr->Get());
-   uint64_t base_data = reinterpret_cast<uintptr_t>(aligned_ptr->base_.get());
-   auto output = aligned_ptr->Reset();
-   uint64_t output_data = reinterpret_cast<uintptr_t>(output.get());
-   uint64_t output_base =reinterpret_cast<uintptr_t>(aligned_ptr->base_.get());
-   ASSERT_EQ(aligned_data, output_data);
-   ASSERT_EQ(output_base, 0);
-  }
-
-  TEST_F(UtestAlignedPtr, BuildFromData_success) {
-   auto deleter = [](uint8_t *ptr) {
-     delete ptr;
-     ptr = nullptr;
-   };
-   uint8_t* data_ptr = new uint8_t(10);
-   auto aligned_ptr = AlignedPtr::BuildFromData(data_ptr, deleter);
-   uint8_t result = *(aligned_ptr->Get());
-   ASSERT_EQ(result, 10);
-  }
+  aligned_ptr->Reset(data_ptr, deleter);
+  ASSERT_EQ(aligned_ptr->Get(), data_ptr);
 }
+
+TEST_F(UtestAlignedPtr, BuildFromData_success) {
+  auto deleter = [](uint8_t *ptr) {
+    delete ptr;
+    ptr = nullptr;
+  };
+  uint8_t *data_ptr = new uint8_t(10);
+  auto aligned_ptr = AlignedPtr::BuildFromData(data_ptr, deleter);
+  uint8_t result = *(aligned_ptr->Get());
+  ASSERT_EQ(result, 10);
+}
+}  // namespace ge
