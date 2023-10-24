19d30311498836debf6004fdcca3ba9027a77267
guopeian <guopeian1@hisilicon.com>
2023-10-24 13:45:20 +0000

    !4225 修改op_execute_context Merge pull request !4225 from guopeian/op_execute_move

---
+++ exe_graph/runtime/op_execute_context.cc
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) Huawei Technologies Co., Ltd. 2023. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "exe_graph/runtime/op_execute_context.h"
+#include "exe_graph/memory/gert_mem_allocator.h"
+
+namespace gert {
+  void *OpExecuteContext::MallocWorkspace(const size_t size) {
+    auto memory_vec =
+        GetOutputPointer<std::vector<ge::MemBlock *>>(
+            static_cast<size_t>(OpExecuteOutputIndex::kBlockMemory));
+    if (memory_vec == nullptr) {
+      return nullptr;
+    }
+    const size_t input_num = GetComputeNodeInputNum();
+    const size_t output_num = GetComputeNodeOutputNum();
+    auto allocator_info =
+        GetInputValue<memory::GertMemAllocator *>(input_num + output_num +
+        static_cast<size_t>(OpExecuteInputExtendIndex::kAllocate));
+    if ((allocator_info == nullptr) || (allocator_info->allocator == nullptr)) {
+      return nullptr;
+    }
+    auto mem_block = allocator_info->allocator->Malloc(size);
+    if (mem_block == nullptr) {
+      return nullptr;
+    }
+    (void)memory_vec->emplace_back(mem_block);
+    return mem_block->GetAddr();
+  }
+
+  void OpExecuteContext::FreeWorkspace() {
+    auto memory_vec =
+        GetOutputPointer<std::vector<ge::MemBlock *>>(
+            static_cast<size_t>(OpExecuteOutputIndex::kBlockMemory));
+    if (memory_vec == nullptr) {
+      return;
+    }
+    for (size_t i = 0UL; i < memory_vec->size(); i++) {
+      if (memory_vec->at(i) != nullptr) {
+        memory_vec->at(i)->Free();
+      }
+    }
+    memory_vec->clear();
+  }
+}
---
+++ tests/ut/exe_graph/op_execute_context_unittest.cc
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2022 Huawei Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "exe_graph/runtime/op_execute_context.h"
+#include "exe_graph/memory/gert_mem_allocator.h"
+#include "graph/ge_error_codes.h"
+#include <gtest/gtest.h>
+#include "faker/kernel_run_context_faker.h"
+#include "faker/allocator_faker.h"
+#include "exe_graph/runtime/storage_shape.h"
+#include "register/kernel_registry.h"
+namespace gert {
+class OpExecuteContextUT : public testing::Test {};
+TEST_F(OpExecuteContextUT, MallocFreeWorkSpaceOk) {
+  std::vector<ge::MemBlock *> mem_block;
+  ge::Allocator *allocator = new AllocatorFaker();
+  memory::GertMemAllocator gert_allocator;
+  gert_allocator.allocator = allocator;
+  gert_allocator.placement = kOnDeviceHbm;
+  auto context_holder = KernelRunContextFaker()
+                            .IrInputNum(2)
+                            .IrInstanceNum({1, 1})
+                            .KernelIONum(7, 1)
+                            .NodeIoNum(2, 1)
+                            .Inputs({nullptr, nullptr, nullptr, &gert_allocator, nullptr, nullptr, nullptr})
+                            .Outputs({&mem_block})
+                            .Build();
+  auto context = context_holder.GetContext<OpExecuteContext>();
+  ASSERT_NE(context, nullptr);
+  ASSERT_NE(context->MallocWorkspace(1024), nullptr);
+  context->FreeWorkspace();
+  delete allocator;
+}
+}
